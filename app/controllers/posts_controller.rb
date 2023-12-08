7d62813022aab4fdf82cd3a1868a6b01b945ddf1
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-12-08 17:14:55 +0100

    Added the basic routes, controllers and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+
+    def index
+        @user = User.find(params[:user_id])
+        @posts = User.posts
+    end
+
+    def show
+        @user = User.find(params[:user_id])
+        @post = User.posts.find(params[:id])
+    end
+end
