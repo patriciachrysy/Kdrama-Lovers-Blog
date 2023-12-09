9c0b4a4197151c097ea461c2f01c135906a48721
Manezeu Patricia <pmanezeu@gmail.com>
2023-12-09 20:22:06 +0100

    Added the post and comment form and like features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,29 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @author = current_user
+    @post = @author.posts.new(post_params)
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+    if @post.save
+      redirect_to user_post_path(@author, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
