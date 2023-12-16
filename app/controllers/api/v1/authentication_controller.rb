c9a48099858ca12db6cc91966ca43b0d14fc9a8c
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-12-16 14:33:36 +0300

    Fix authentication_controller error

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,13 +1,17 @@
-def login
-  @user = User.find_by(email: params[:email])
-  if @user&.valid_password?(params[:password])
-    encoded_token = jwt_encode(user_id: @user.id)
-    if encoded_token[:errors]
-      render json: { error: encoded_token[:errors] }, status: :unauthorized
+class Api::V1::AuthenticationController < ActionController::API
+  include JsonWebToken
+
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user&.valid_password?(params[:password])
+      encoded_token = jwt_encode(user_id: @user.id)
+      if encoded_token[:errors]
+        render json: { error: encoded_token[:errors] }, status: :unauthorized
+      else
+        render json: { token: encoded_token }, status: :ok
+      end
     else
-      render json: { token: encoded_token }, status: :ok
+      render json: { error: 'Unauthorized!' }, status: :unauthorized
     end
-  else
-    render json: { error: 'Unauthorized!' }, status: :unauthorized
   end
 end
